bc4a6d7051d8a38ba00f8e7b47386cad282d2493
kristakernodle <kristakernodle@gmail.com>
2019-07-05 16:03:40 -0400

    Added a variety of auxiliary/helper functions. Some general (directory searches and csv writing) some specific (Harvey's smooth function)

---
+++ auxFunc.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Auxiliary Functions
+
+Created on Fri Jul  5 12:50:49 2019
+
+@author: kkrista
+"""
+
+##### Auxiliary Functions
+# This file holds all auxiliary ("helper") functions used in the automated scoring
+# software developed in the Leventhal lab. 
+
+### Set up
+from pathlib import Path
+import numpy as np
+
+### Functions to manipulate DLC data
+def smooth(x,window_len=15,window='hanning'):
+    if window_len<3:
+        return x
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+### General Functions
+
+def directoryContents(directory,allSubDir=True):
+    # This function searches a directory and returns the files in it.
+    
+    # INPUT:
+        # directory - path of directory you want all files from
+        # allSubDir - boolean defining whether to search through subdirectories
+        #        Default: True. All files in directory and its subdirectories are listed
+        #        When set to False, files in directory and directories in directory will be listed
+    
+    # OUTPUT:
+        # file_list - list of all files
+        # dir_list - list of all directories (only output with allSubDir=False)
+   
+    # Check that input is a directory
+    dirpath=Path(directory)
+    assert(dirpath.is_dir())
+    
+    # Initialize variables
+    file_list = []
+    dir_list = []
+    
+    # Begin searching through directory
+    for x in dirpath.iterdir():
+        
+        # for x is a file
+        if x.is_file():
+            
+            # ignore 'invisible' files
+            if '._' in x.as_posix():
+                continue
+            # add file to file_list
+            else:
+                file_list.append(x.as_posix())
+                
+        # for x is a directory
+        elif x.is_dir():
+            
+            # Search through subdirectories           
+            if allSubDir == True:
+                file_list.extend(directoryContents(x))
+                
+            # Save subdirectories, but don't search through them
+            elif allSubDir == False:
+                dir_list.append(x.as_posix())
+
+    # Define function output       
+    if allSubDir == True:
+        return file_list
+    else:
+        return file_list, dir_list
+            
+def writeToCSV(saveFullFilename,itemsList):
+    with open(saveFullFilename,'w') as f:
+        for item in itemsList:
+            f.writelines("%s," % entry for entry in item)
+            f.write("\n")
+        f.close()
