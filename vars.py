367478d45e285dc02d2dd0bc010035cf3e687bf6
kristakernodle <kristakernodle@gmail.com>
2019-07-05 12:34:09 -0400

    adding file with user defined variables

---
+++ vars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Variables
+
+Created on Fri Jul  5 12:27:07 2019
+
+@author: Krista Kernodle
+"""
+
+## Variables
+# This file is designed to hold all user identified variables necessary for
+# use with the automated scoring software developed in the Leventhal lab.
+
+
+## Directory paths
+
+# Pathway to the DeepLabCut direct view .csv files
+pathToDirect="/Users/kkrista/Documents/GitHub/automatedScoring/sampleData/R0189_20170924a/R0189_20170924a_direct"
+
+# Pathway to the DeepLabCut mirror view .csv files
+pathToMirror="/Users/kkrista/Documents/GitHub/automatedScoring/sampleData/R0189_20170924a/R0189_20170924a_left"
