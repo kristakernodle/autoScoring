89c81db6d72552fbfcc5abcbbbf9366e27cd834e
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2019-07-07 14:01:45 -0400

    Tested data sets

--- vars.py
+++ vars.py
@@ -16,7 +16,7 @@ Created on Fri Jul  5 12:27:07 2019
 ## Directory paths
 
 # Pathway to the DeepLabCut direct view .csv files
-pathToDirect="/Users/Krista/Documents/GitHub_Repos/automatedScoring/sampleData/R0189_20170924a/R0189_20170924a_direct"
+pathToDirect="/Users/Krista/Documents/GitHub_Repos/automatedScoring/sampleData/R0186_20170815a/R0186_20170815a_direct"
 
 # Pathway to the DeepLabCut mirror view .csv files
-pathToMirror="/Users/Krista/Documents/GitHub_Repos/automatedScoring/sampleData/R0189_20170924a/R0189_20170924a_left"
+pathToMirror="/Users/Krista/Documents/GitHub_Repos/automatedScoring/sampleData/R0186_20170815a/R0186_20170815a_left"
