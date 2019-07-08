6a19632beccb785e8f6548979322f523b4e910c9
kristakernodle <kristakernodle@gmail.com>
2019-07-08 09:41:03 -0400

    added Harvey's findTriggerFrame function and updated comments for sigP function

--- manipDLCFunc.py
+++ manipDLCFunc.py
@@ -18,22 +18,6 @@ import auxFunc
 
 ### Define Functions
 
-def smooth(x,window_len=15,window='hanning'):
-    # This is Harvey's function and I haven't put time into figuring out what 
-    # it's doing yet
-
-    if window_len<3:
-        return x
-
-    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
-    if window == 'flat': #moving average
-        w=np.ones(window_len,'d')
-    else:
-        w=eval('np.'+window+'(window_len)')
-
-    y=np.convolve(w/w.sum(),s,mode='valid')
-    return y
-
 def readDLC(f,bodypart,pawPref):
     # This function reads in DLC data for a single bodypart
     
@@ -81,7 +65,21 @@ def readDLC(f,bodypart,pawPref):
     return pd.read_csv(f,header=2,names=['x','y','pval'],usecols=cols)
 
 def sigP(bodypartList_direct,bodypartList_mirror,startFrame,endFrame):
-    # Find the
+    # Find and return the DLC data points that have a significant P-value (>0.95)
+    
+    # INPUT:
+        # bodypartList_direct - python list containing [x,y,pval] for direct view
+        #     (read in from readDLC function above)
+        # bodypartList_mirror - same as bodypartList_direct, except for mirror view
+        # startFrame - first frame number of interest
+        # endFrame - last frame number of interest
+        
+    # OUTPUT:
+        # xDir - x-values from direct view with p>=0.95
+        # yDir - y-values from direct view with p>=0.95
+        # xMir - x-values from mirror view with p>=0.95
+        # yMir - y-values from mirror view with p>=0.95
+        
     xDir=yDir=xMir=yMir=[]
     for frameNum in range(startFrame,endFrame):
         if bodypartList_direct.pval[frameNum] >= 0.95:
@@ -98,6 +96,37 @@ def sigP(bodypartList_direct,bodypartList_mirror,startFrame,endFrame):
             yMir.append(0)
             
     return xDir,yDir,xMir,yMir
+
+def findTriggerFrame(csvFileName, csvFileNameMirror):
+    # This is Harvey's function and I haven't put a lot of time into sorting out what it does yet
+	digit2xvalue = []
+	digit2pvalue = []
+
+	with open(csvFileNameMirror) as f:
+		reader = csv.reader(f)
+		for i in reader:
+			digit2xvalue.append(i[28])
+			digit2pvalue.append(i[30])
+
+	digit2xvalue = digit2xvalue[3:]
+	digit2pvalue = digit2pvalue[3:]
+	digit2xvalue = [float(i) for i in digit2xvalue]
+	digit2pvalue = [float(i) for i in digit2pvalue]
+	digit2xvalue = digit2xvalue[0:1290]
+	for i in range(0, 1290):
+		if digit2pvalue[i] < 0.75:
+			digit2xvalue[i] = 0
+
+	for i in range(1, 1280):
+		if digit2xvalue[i] != 0:
+
+			if digit2xvalue[i+1] == 0 or digit2xvalue[i+2] == 0 or digit2xvalue[i+3] == 0 or digit2xvalue[i+4] == 0 or digit2xvalue[i+5] == 0 or digit2xvalue[i] > 250: 
+				
+				digit2xvalue[i] = 0
+
+	for i in range(1, 1290):
+		if digit2xvalue[i] != 0:
+			return i
 
 def withinDistThresh(distThresh,xDir,yDir,xDirAvg,yDirAvg,xMir,yMir,xMirAvg,yMirAvg):
     # This function calculates whether a bodypart remains within a specified
@@ -124,3 +153,19 @@ def withinDistThresh(distThresh,xDir,yDir,xDirAvg,yDirAvg,xMir,yMir,xMirAvg,yMir
         return True
     else:
         return False
+
+def smooth(x,window_len=15,window='hanning'):
+    # This is Harvey's function and I haven't put time into figuring out what 
+    # it's doing yet
+
+    if window_len<3:
+        return x
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
